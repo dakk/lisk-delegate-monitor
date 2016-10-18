cb5f2bd2287c12a51de3494ba1d80315ad1aace9
Davide Gessa <gessadavide@gmail.com>
2016-10-18 09:57:23 +0200

    Delegate list and balances

--- controllers.js
+++ controllers.js
@@ -1,17 +1,55 @@
 var express		= require ('express');
 var request     = require ('request');
+var log			= require ('./log');
 var config      = require ('./config.json');
 
 var delegateList = [];
+var stats = {
+	delegates: 0,
+	mined: 0,
+	shift: 0
+};
+var balances = {};
 
 exports.update = function () {
-	request('http://' + config.node + '/api/delegates/?limit=200&offset=0&orderBy=rate:asc', function (error, response, body) {
+	log.debug ('Data', 'Updating data...');
+	request('http://' + config.node + '/api/delegates/?limit=101&offset=0&orderBy=rate:asc', function (error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var data = JSON.parse(body);
 
-			delegateList = [];
+			var delegateList2 = [];
+			var stats2 = { delegates: 0, mined: 0, shift: 0 };
+
+	   		for (var i = 0; i < data.delegates.length; i++) {
+				if (config.lobby.indexOf (data.delegates[i].username) != -1) {
+					stats2.delegates += 1;
+					stats2.mined += data.delegates[i].producedblocks;
+					delegateList2.push (data.delegates[i]);
+				}
+			}
+
+			for (var d in balances) {
+				stats2.shift += Math.floor (balances[d]);
+			}
+
+			delegateList = delegateList2;
+			stats = stats2;
+
+			log.debug ('Data', 'Data updated.');
 		}
 	});
+};
+
+exports.updateBalances = function () {
+	log.debug ('Data', 'Updating balance data...');
+	for (var i = 0; i < delegateList.length; i++) {
+		request ('http://' + config.node + '/api/accounts?address=' + delegateList[i].address, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var data = JSON.parse(body);
+				balances [data.account.address] = data.account.balance / 100000000;
+			}
+		});
+	}
 };
 
 
@@ -23,7 +61,7 @@ var checkLogin = function (req, res, next) {
 };
 
 router.get('/', checkLogin, function (req, res) {
-	res.render ('users', { users: data, title: 'Pending '+provider+' verify' });
+	res.render ('index', { delegates: delegateList, stats: stats, balances: balances });
 });
 
 
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 var controllers = require ('./controllers');
 var config      = require ('./config.json');
+var log 		= require ('./log');
 
 var morgan      = require ('morgan');
 var express     = require ('express');
 
 
 process.on('uncaughtException', function (err) {
-	log.critical ('Except', err.stack);
+	console.log ('Except', err.stack);
 });
 
 
 /* Start the update loop */
 controllers.update ();
 setInterval (controllers.update, 10000);
+setTimeout (controllers.updateBalances, 5000);
+setInterval (controllers.updateBalances, 60000);
 
 
 /* Server */
@@ -21,5 +24,7 @@ app.use (morgan ('route', { skip: function (req, res) { return (req.method == 'O
 
 app.set ('views', __dirname + '/views');
 app.set ('view engine', 'jade');
-app.use ('/', controllers);
+app.use ('/', controllers.router);
+
+log.debug ('Server', 'Listening on port: ' + config.port);
 var server = app.listen (config.port);
